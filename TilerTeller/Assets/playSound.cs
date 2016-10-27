61093089646250e6b7ad4e9de293a263db1db16a
Aaron Ishibashi <tofuaishi@gmail.com>
2016-10-26 22:14:54 -0700

    Wwise Implementation
    1. Successfully imported the Wwise-Unity Integration Package for Wwise
    version 2016.1.3.
    
    2. Created a Wwise session titled “TilerTellerWwise” put into the main
    project directory. This is where I (Aaron) am importing sounds and
    adjusting their behavior. The sound bank is stored in Assets>Streaming
    Assets folder.
    
    3. Created a new script called “WwiseEventSend.cs” which calls the
    event from Wwise’s sound bank. Adjusted the playSound.cs script to
    activate Wwise event.
    
    Both the title background music and the button press sound are
    implemented successfully on my (Aaron’s) end.

--- TilerTeller/Assets/playSound.cs
+++ TilerTeller/Assets/playSound.cs
@@ -3,15 +3,22 @@ using System.Collections;
 
 public class playSound : MonoBehaviour {
 
-	private AudioSource audio{get{ return GetComponent<AudioSource> ();}}
+	public string wwiseEvent;
+
+	//Commented to test Wwise Audio Engine Instead
+	//private AudioSource audio{get{ return GetComponent<AudioSource> ();}}
 
 	void Awake(){
+		
 		DontDestroyOnLoad (gameObject);
 	}
 	
 	public void PlaySound(){
-		if (audio != null) {
+		//Commented to test Wwise Audio Engine Instead
+		/*if (audio != null) {
 			audio.Play ();
 		}
+		*/
+		AkSoundEngine.PostEvent (wwiseEvent,this.gameObject);
 	}
 }
